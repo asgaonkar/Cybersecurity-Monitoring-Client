15574703fe593a809d0621782ca8914e28c3e295
Atit S Gaonkar <atit.sgaonkar@gmail.com>
2021-04-17 21:17:56 -0700

    Handling Pi Exceptions

--- Code/RunOnReboot/requirementSniffThread.py
+++ Code/RunOnReboot/requirementSniffThread.py
@@ -300,25 +300,28 @@ class requirementSniffThread(threading.Thread):
    def getStaticInfo(self):
       staticInformation = {}
       try:
-         cpuInfo = CPUInfo.getCPUInfo()
-         osInfo = OSInfo.getOSInfo()
-         staticInformation["MAC Address"] = NetworkInfo.getMAC()
-         staticInformation["Hostname"] = osInfo["Hostname"]
+         staticInformation["MAC Address"] = NetworkInfo.getMAC()         
+      except:
+         pass
+      try:
+         cpuInfo = CPUInfo.getCPUInfo()                  
          staticInformation["Serial Number"] = cpuInfo['Serial'][0]
          staticInformation["Hardware"] = cpuInfo['Hardware'][0]
          staticInformation["Manufacturer"] = ManufatcurerInfo.getManufaturerInfo(cpuInfo["Revision"][0])
+      except Exception as e:
+         pass
+      try:
+         osInfo = OSInfo.getOSInfo()
          staticInformation["OS"] = {}
+         staticInformation["Hostname"] = osInfo["Hostname"]
          staticInformation["OS"]["Name"] = osInfo["NAME"]
          staticInformation["OS"]["Type"] = osInfo["ID_LIKE"]
          staticInformation["OS"]["Version Number"] = osInfo["VERSION_ID"]
          staticInformation["OS"]["Architecture"] = osInfo["Architecture"]
          staticInformation["OS"]["Version"] = osInfo["OS Version"]
          staticInformation["OS"]["Release"] = osInfo["OS Release"]
-      except KeyboardInterrupt as e:
-         print("Show Error: ",e)
-         sys.exit()        
-      finally:      
-         return staticInformation
+      except Exception as e:
+         pass
       return staticInformation
 
    def catchFile(self):            
